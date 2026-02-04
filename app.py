cbf6828c3846ba2879f9a9a11990833aab3794e5
nupoorbande98-hub <nupoorbande98@gmail.com>
2026-02-04 21:18:52 +0530

    app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def set_background(image_file):
 
 # ---------- LOGIN PAGE ----------
 def login_page():
-    set_background("assets/images/background.jpg")
+set_background("background.jpg")
 
     # Center using Streamlit layout (safe way)
     col1, col2, col3 = st.columns([1, 1.2, 1])
@@ -125,4 +125,5 @@ else:
         show_order()
 
     elif page == "Prediction":
+
         show_prediction()
